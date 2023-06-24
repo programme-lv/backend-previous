568db5b1c5c0cee447bd978de7e07165a85d77eb
KrisjanisP <krisjanispetrucena@gmail.com>
2023-06-24 19:34:50 +0300

    add UpdatedAt field to user

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,11 +1,14 @@
 package models
 
+import "time"
+
 type User struct {
-	ID             int64  `db:"id"`
-	Username       string `db:"username"`
-	HashedPassword string `db:"hashed_password"`
-	Email          string `db:"email"`
-	FirstName      string `db:"first_name"`
-	LastName       string `db:"last_name"`
-	CreatedAt      string `db:"created_at"`
+	ID             int64      `db:"id"`
+	Username       string     `db:"username"`
+	HashedPassword string     `db:"hashed_password"`
+	Email          string     `db:"email"`
+	FirstName      string     `db:"first_name"`
+	LastName       string     `db:"last_name"`
+	CreatedAt      time.Time  `db:"created_at"`
+	UpdatedAt      *time.Time `db:"updated_at"`
 }
