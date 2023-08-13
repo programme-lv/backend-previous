449ff349eab007ee9c20b9c731d24deb52c143d8
KrisjanisP <krisjanispetrucena@gmail.com>
2023-08-13 21:00:16 +0300

    add test test

---
+++ cmd/server/main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func TestAbs(t *testing.T) {
+	got := Abs(-1)
+	if got != 1 {
+		t.Errorf("Abs(-1) = %d; want 1", got)
+	}
+}
