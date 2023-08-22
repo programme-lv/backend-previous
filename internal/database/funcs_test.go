9c32e58ccd3a9dde438c4d582f83f03bcb77338e
KrisjanisP <krisjanispetrucena@gmail.com>
2023-08-22 17:58:00 +0300

    TestMain

--- internal/database/funcs_test.go
+++ internal/database/funcs_test.go
@@ -1,18 +1,31 @@
 package database
 
 import (
+	"log"
+	"os"
+	"sync"
 	"testing"
 
 	"github.com/programme-lv/backend/internal/database/testdb"
 )
 
-func TestCreateUser(t *testing.T) {
-	dbProvider, err := testdb.NewPostgresTestcontainerProvider()
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Cleanup(func() { dbProvider.Close() })
+var (
+	once       sync.Once
+	dbProvider testdb.TestDBProvider
+)
 
+func TestMain(m *testing.M) {
+	var err error
+	dbProvider, err = testdb.NewPostgresTestcontainerProvider()
+	if err != nil {
+		log.Fatal(err)
+	}
+	exitCode := m.Run()
+	dbProvider.Close()
+	os.Exit(exitCode)
+}
+
+func TestCreateUser(t *testing.T) {
 	db := dbProvider.GetTestDB()
 	// asert db is not null
 	if db == nil {
